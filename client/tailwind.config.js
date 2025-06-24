4a29538475e7fb2e915b7aea852e9f3ad271d4aa
samhith <samhith.mr@gmail.com>
2025-06-24 07:50:33 +0530

    setup of basic frontend applciation with react and vite

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2BB5FF',
+        secondary: '#EF863E',
+      },
+    },
+  },
+  plugins: [],
+}
+
